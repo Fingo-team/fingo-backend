89214b6cc474a2dae2d71a05557dca38c75babc8
kthanterran@gmail.com <kthanterran@gmail.com>
2016-11-29 14:43:52 +0900

    오타수정.
    settings.py >> sys.args -> sys.argv

--- django_app/fingo/settings.py
+++ django_app/fingo/settings.py
@@ -98,7 +98,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': os.path.join(BASE_DIR, 'db.fingo'),
         }
     }
 else:
