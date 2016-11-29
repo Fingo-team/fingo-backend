1bbd3d635a1eec9611937c4c4fdc9dd921d30f88
kthanterran@gmail.com <kthanterran@gmail.com>
2016-11-30 00:41:49 +0900

    deploy test

--- django_app/fingo/settings.py
+++ django_app/fingo/settings.py
@@ -108,7 +108,7 @@ if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.fingo'),
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
 else:
