1c9ba4dbddbe4aca36df89869e60ce5fb3aa32fe
kthanterran@gmail.com <kthanterran@gmail.com>
2016-12-06 20:07:31 +0900

    merge add comment function and Facebook Login

--- django_app/movie/views.py
+++ django_app/movie/views.py
@@ -1,7 +1,5 @@
 from rest_framework import status
 from rest_framework.settings import api_settings
-from rest_framework.pagination import CursorPagination
-from rest_framework.generics import ListAPIView
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
