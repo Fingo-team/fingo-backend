b92c5f83fa79f269ebfd3d12535ceedf321b70a9
kthanterran@gmail.com <kthanterran@gmail.com>
2016-12-07 19:33:58 +0900

    revision url`

--- django_app/fingo_statistics/urls.py
+++ django_app/fingo_statistics/urls.py
@@ -3,5 +3,7 @@ from fingo_statistics import views
 
 
 urlpatterns = [
-    url(r"userdetail/$", views.UserDetailView.as_view())
+    url(r"user/detail/$", views.UserDetailView.as_view()),
+    url(r"user/comments/$", views.UserDetailView.as_view()),
+    url(r"user/watched_movies/$", views.UserDetailView.as_view()),
 ]
