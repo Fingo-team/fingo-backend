115f8b18e0edd0106f29559a81c01b4dd37e5eec
kthanterran@gmail.com <kthanterran@gmail.com>
2016-12-07 22:19:02 +0900

    revision typing error urls of movie and fingo_statistics.

--- django_app/fingo_statistics/urls.py
+++ django_app/fingo_statistics/urls.py
@@ -3,7 +3,7 @@ from fingo_statistics import views
 
 
 urlpatterns = [
-    url(r"user/detail/$", views.UserDetailView.as_view()),
-    url(r"user/comments/$", views.UserComments.as_view()),
-    # url(r"user/watched_movies/$", views.UserDetailView.as_view()),
+    url(r"^user/detail/$", views.UserDetailView.as_view()),
+    url(r"^user/comments/$", views.UserComments.as_view()),
+    # url(r"^user/watched_movies/$", views.UserDetailView.as_view()),
 ]
