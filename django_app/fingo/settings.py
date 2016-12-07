c0ac133d09844a0b4c8c7c6d064f5778ebe2fb5d
Pando <ndjman7@gmail.com>
2016-12-07 15:51:08 +0900

    UserCommentSerilizer add 'score' field
    CommentSerializer field에 score를 추가했다.

--- django_app/fingo/settings.py
+++ django_app/fingo/settings.py
@@ -76,7 +76,7 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.TokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.CursorPagination',
-    'PAGE_SIZE': 1
+    'PAGE_SIZE': 10
 }
 
 MIDDLEWARE = [
