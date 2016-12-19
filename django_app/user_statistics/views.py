2e1e56ea50e9f966d4583963183a0551f607be56
kthanterran@gmail.com <kthanterran@gmail.com>
2016-12-19 16:10:09 +0900

    revision user statistics.
    /statistics/all/을 제외한 모든 url 삭제.
    StatisticsAll를 제외한 모든 class 삭제.

--- django_app/user_statistics/views.py
+++ django_app/user_statistics/views.py
@@ -7,61 +7,6 @@ from user_statistics.serializations import StatisticsScoresSerializer, Statistic
     StatisticsGenreSerializer, StatisticsNationSerializer
 
 
-class StatisticsScores(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        user_statistics, created = UserStatistics.objects.get_or_create(user=user)
-        user_scores, created = UserScores.objects.get_or_create(user_statistics=user_statistics)
-        serial = StatisticsScoresSerializer(user_scores)
-        return Response(serial.data, status=status.HTTP_200_OK)
-
-
-class StatisticsActors(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        user_statistics, created = UserStatistics.objects.get_or_create(user=user)
-        user_actors = UserActor.objects.filter(user_statistics=user_statistics).order_by('-count')
-        serial = StatisticsActorSerializer(user_actors, many=True)
-        return Response(serial.data, status=status.HTTP_200_OK)
-
-
-class StatisticsDirectors(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        user_statistics, created = UserStatistics.objects.get_or_create(user=user)
-        user_directors = UserDirector.objects.filter(user_statistics=user_statistics).order_by('-count')
-        serial = StatisticsDirectorSerializer(user_directors, many=True)
-        return Response(serial.data, status=status.HTTP_200_OK)
-
-
-class StatisticsGenres(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        user_statistics, created = UserStatistics.objects.get_or_create(user=user)
-        user_genres = UserGenre.objects.filter(user_statistics=user_statistics).order_by('-count')
-        serial = StatisticsGenreSerializer(user_genres, many=True)
-        return Response(serial.data, status=status.HTTP_200_OK)
-
-
-class StatisticsNations(APIView):
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request, *args, **kwargs):
-        user = request.user
-        user_statistics, created = UserStatistics.objects.get_or_create(user=user)
-        user_nations = UserNation.objects.filter(user_statistics=user_statistics).order_by('-count')
-        serial = StatisticsNationSerializer(user_nations, many=True)
-        return Response(serial.data, status=status.HTTP_200_OK)
-
-
 class StatisticsAll(APIView):
     permission_classes = (IsAuthenticated,)
 
